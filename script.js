6907c542963d67c6119655d87590ee80abd39f83
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-16 15:18:44 +0100

    Sanificazione input

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 /*  Chiedere all’utente di inserire una parola
     Creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata */
 
+//Trasformo l'input dell'utente in una variabile iniziale
+
+let word_input = prompt("Inserisca una parola");
+
+/* Sanificazione dell'input dell'utente
+Siccome l'input deve essere una parola, vengono cercati tutti i caratteri che non sono lettere (numeri inclusi) e vengono cambiati in uno spazio nullo, eleminandoli dall'input */
+
+let word_clean = word_input.replace(/[^a-zA-Z]/g, "");
+   
