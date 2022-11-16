0c7d2d1e5332714318095cebf92517107a1cc14d
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-16 16:23:19 +0100

    Fine della parte del palindromo

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 /*  CONSEGNA: Chiedere all’utente di inserire una parola
     Creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata */
 
-//Trasformazione  dell'input dell'utente in una variabile iniziale
+PalindromeCheck()
 
-let input_word = prompt("Inserisca una parola");
+function PalindromeCheck() {
 
-/* SANIFICAZIONE DELL'INPUT DELL'UTENTE
-Siccome l'input deve essere una parola, vengono cercati tutti i caratteri che non sono lettere (numeri inclusi) e vengono cambiati in uno spazio nullo, eleminandoli dall'input */
+    //Trasformazione  dell'input dell'utente in una variabile iniziale
 
-let clean_word = input_word.replace(/[^a-zA-Z]/g, "");
-   
-// Per operare in modo efficiente, è necessario trasformare tutte le lettere dell'input in lettere minuscole
+    let input_word = prompt("Inserisca una parola");
 
-let lowercase_word = clean_word.toLocaleLowerCase();
+    /* SANIFICAZIONE DELL'INPUT DELL'UTENTE
+    Siccome l'input deve essere una parola, vengono cercati tutti i caratteri che non sono lettere (numeri inclusi) e vengono cambiati in uno spazio nullo, eleminandoli dall'input */
 
-/* Per verificare se la parola dell'utente è un palindromo oppure no, è necessario suddividere le lettere della parola dell'utente in un array (questo grazie alla funzione .split),
-dopodichè è necessario creare una copia del primo array, invertire l'ordine delle lettere, e verificare se la posizione delle lettere è cambiato o meno */
+    let clean_word = input_word.replace(/[^a-zA-Z]/g, "");
 
-let normal_letters = lowercase_word.split("");
+    // Per operare in modo efficiente, è necessario trasformare tutte le lettere dell'input in lettere minuscole
 
-/* Creazione copia del primo array. Viene fatta una copia siccome il reverese method è distruttivo, rendendo impossibile un confronto con l'ordine dell'array originale senza
-la creazione di una copia */
+    let lowercase_word = clean_word.toLocaleLowerCase();
 
-let reversed_letters = []
+    /* Per verificare se la parola dell'utente è un palindromo oppure no, è necessario suddividere le lettere della parola dell'utente in un array (questo grazie alla funzione .split),
+    dopodichè è necessario creare una copia del primo array, invertire l'ordine delle lettere, e verificare se la posizione delle lettere è cambiato o meno */
 
-for (i = 0; i < normal_letters.length; i++) {
+    let normal_letters = lowercase_word.split("");
 
-    reversed_letters[i] = normal_letters[i]
+    /* Creazione copia del primo array. Viene fatta una copia siccome il reverese method è distruttivo, rendendo impossibile un confronto con l'ordine dell'array originale senza
+    la creazione di una copia */
 
+    let reversed_letters = []
+
+    for (i = 0; i < normal_letters.length; i++) {
+
+        reversed_letters[i] = normal_letters[i]
+
+    }
+
+    // Inversione dell'ordine delle lettere nella copia
+
+    reversed_letters = reversed_letters.reverse();
+
+    // Confronto dell'ordine normale con l'ordine inverso delle lettere
+
+    for (i = 0; i < normal_letters.length; i++) {
+
+        //Se le lettere non sono uguali, vuol dire che la parola non è palindroma 
+
+        if (normal_letters[i] != reversed_letters[i]) {
+
+            console.log("La parola " + lowercase_word + " non è palindroma!");
+            break;
+
+            //Altrimenti, il confronto delle singole lettere continua fino alla fine. Se il ciclo raggiunge la fine dei due array con successo, se ne deduce che la parola è palindroma
+
+        } if (i == normal_letters.length - 1) {
+
+            console.log("La parola " + lowercase_word + " è palindroma!");
+        }
+    }
 }
-
-// Inversione dell'ordine delle lettere nella copia
-
-reversed_letters = reversed_letters.reverse();
-
-console.log(normal_letters)
-console.log(reversed_letters)
-// Confronto dell'ordine normale con l'ordine inverso delle lettere
-
-if (normal_letters[0] == reversed_letters[0]) {console.log(true)}
-else {console.log(false)}
-    
